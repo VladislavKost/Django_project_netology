0f4473fcda75baf6dc0d14d0393d8148e076644b
Kostrov Vladislav <Kostrov-v-s@yandex.ru>
2024-01-02 17:24:17 +0500

    Finish orm_migrations task. Add django toolbar.
    Завершил задание с приложением orm_migrations. Поменял отношение между учителями и учениками на многие ко многим, добавил обратную связь. Изменил json, чтобы данные загружались корректно с измененной моделью.
    Добавил django debug toolbar.

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "school",
 ]
 
@@ -47,9 +48,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "website.urls"
+
+INTERNAL_IPS = ["127.0.0.1"]
 
 TEMPLATES = [
     {
@@ -76,6 +80,8 @@ DATABASES = {
         "NAME": "netology_orm_migrations",
         "HOST": "127.0.0.1",
         "PORT": "5432",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
     }
 }
 
