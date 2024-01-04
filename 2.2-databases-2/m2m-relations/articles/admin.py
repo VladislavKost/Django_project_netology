1f0570ad4f5b9022b5a6b993a0470696a5075b5b
Kostrov Vladislav <Kostrov-v-s@yandex.ru>
2024-01-04 14:45:10 +0500

    Finish second task m2m-realtions
    Завершил задание 2.2 со статьями. Добавил модели, для отображения статей на сайте, добавил inline и formset для удобного отображения и воода данных в админ панели. Добавил ограничения на уникальность пары статья-тег.
    Добавил ограничения на выбор основного тега. Добавил правильную сортировку.

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ArticleTagsInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count_mains = 0
+        for form in self.cleaned_data:
+            if form.get("is_main", False):
+                count_mains += 1
+        if count_mains > 1:
+            raise ValidationError("Только один тег может быть основным")
+        if count_mains == 0:
+            raise ValidationError("Необходимо выбрать основной тег")
+        return super().clean()
+
+
+class ArticleTagsInline(admin.StackedInline):
+    model = Scope
+    formset = ArticleTagsInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["title", "published_at"]
+    inlines = [
+        ArticleTagsInline,
+    ]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
+class Tag(models.Model):
+
+    name = models.CharField(max_length=50, verbose_name="Название тега")
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+        ordering = ["-name"]
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name="Название")
@@ -11,10 +24,32 @@ class Article(models.Model):
         blank=True,
         verbose_name="Изображение",
     )
+    tags = models.ManyToManyField(Tag, through="Scope")
 
     class Meta:
         verbose_name = "Статья"
         verbose_name_plural = "Статьи"
+        ordering = ["-published_at"]
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, verbose_name="Статья", related_name="scopes"
+    )
+    tag = models.ForeignKey(
+        Tag, on_delete=models.CASCADE, verbose_name="Тег", related_name="scopes"
+    )
+    is_main = models.BooleanField(default=False, verbose_name="Основной")
+
+    class Meta:
+        verbose_name = "Добавить тег"
+        verbose_name_plural = "Добавить теги"
+        ordering = ["-is_main", "-tag"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["article", "tag"], name="unique_article_tag"
+            )
+        ]
