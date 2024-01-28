82b8308671515f55e807c2d1e592e650b4721895
Владислав Костров <vladislav.kostrov@rydlab.ru>
2024-01-28 14:43:54 +0500

    Update serializers and views to get all needed requests

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "logistic",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,8 @@ DATABASES = {
         "NAME": "netology_stocks_products",
         "HOST": "127.0.0.1",
         "PORT": "5432",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
     }
 }
 
@@ -127,3 +130,8 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 3,
+}
